ee4c2cf9ca98a3ea1df8f512285c4a2d2db0d192
Tayebe Safari <tssafri@gmail.com>
2019-03-06 15:49:01 +0330

    Added UnitTest

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using exercise_object_oriented.FNHFolder;
+using exercise_object_oriented;
+using FluentNHibernate;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var session = FluentNHibernateHelper.OpenSesseion())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var measurmentKg = new Measurement { Code = 11111, Name = "Kg" };
+                    var measurmentMeter = new Measurement { Code = 22222, Name = "Meter" };
+                    var product1 = new Product { Code = 33333, Name = "Meat", measuerment = measurmentKg };
+                    var product2 = new Product { Code = 44444, Name = "Rice", measuerment = measurmentKg };
+                    var product3 = new Product { Code = 55555, Name = "Textile", measuerment = measurmentMeter };
+                    var product4 = new Product { Code = 55555, Name = "Pipe", measuerment = measurmentMeter };
+
+                    session.SaveOrUpdate(measurmentKg);
+                    session.SaveOrUpdate(measurmentMeter);
+                    session.SaveOrUpdate(product1);
+                    session.SaveOrUpdate(product2);
+                    session.SaveOrUpdate(product3);
+                    session.SaveOrUpdate(product4);
+
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
