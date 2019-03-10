0c7ec90e2ebf2ca0d480eb168adce653e48ed52b
Tayebe Safari <tssafri@gmail.com>
2019-03-10 17:27:13 +0330

    Added Id & property Conventions

---
+++ exercise-object-oriented/Conventions/MyIdConvention.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_object_oriented.Conventions
+{
+    public class MyIdConvention : IIdConvention
+    {
+        public void Apply(IIdentityInstance instanse)
+        {
+            instanse.Column("Id");
+            instanse.GeneratedBy.Guid();
+        }
+    }
+}
---
+++ exercise-object-oriented/Conventions/MyPropertyConvention.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Conventions.Instances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_object_oriented.Conventions
+{
+    class MyPropertyConvention
+    {
+        public void Apply(IPropertyInstance instance)
+        {
+            if (instance.Type.Name == "Code")
+                instance.CustomType<Int64>();
+        }
+    }
+}
