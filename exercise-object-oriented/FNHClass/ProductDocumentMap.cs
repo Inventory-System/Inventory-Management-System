d0f8b431eb26b09531cbc295cc96e62db356ad08
Tayebe Safari <tssafri@gmail.com>
2019-03-11 17:30:26 +0330

    Changed ProductDocumentMap class to Automapping class

--- exercise-object-oriented/FNHClass/ProductDocumentMap.cs
+++ exercise-object-oriented/FNHClass/ProductDocumentMap.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 using FluentNHibernate;
 using FluentNHibernate.Mapping;
+using FluentNHibernate.Automapping.Alterations;
 
 namespace exercise_object_oriented.FNHFolder
 {
-    class ProductDocumentMap : ClassMap<ProductDocument>
+    class ProductDocumentMap : IAutoMappingOverride<ProductDocument>
     {
-        public ProductDocumentMap()
+        public void Override(FluentNHibernate.Automapping.AutoMapping<ProductDocument> mapping)
         {
-            Map(x => x.OrginalPrice);
-            Map(x => x.Piece);
-            Map(x => x.Profit);
-            Map(x => x.SellingPrice);
-            References(x => x.product);
-          //  DiscriminateSubClassesOnColumn("FieldType22");
+            mapping.Map(x => x.OrginalPrice);
+            mapping.Map(x => x.Piece);
+            mapping.Map(x => x.Profit);
+            mapping.Map(x => x.SellingPrice);
+            mapping.References(x => x.product);
             //  References(x => x.document);
             // HasOne(x => x.TDocument);
             //References(x => x.TDocument);
