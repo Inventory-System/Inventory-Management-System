05ae27e004c101b62fb390d1ae51c8deeb8424e2
HMoradiRad <moradirad007@gmail.com>
2019-03-12 11:12:22 +0330

    Fix Error MesurmentMap

--- exercise-object-oriented/MeasurementMap.cs
+++ exercise-object-oriented/MeasurementMap.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
-namespace exercise_object_oriented
+using FluentNHibernate.Mapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace exercise_object_oriented.Mapping
 {
-    class MeasurementMap
+    class MeasurementMap:IAutoMappingOverride<Measurement>
     {
+        public void Override(FluentNHibernate.Automapping.AutoMapping<Measurement> mapping)
+        {
+            mapping.Map(x => x.Name).Length(20).Not.Nullable();
+        }
     }
 }
