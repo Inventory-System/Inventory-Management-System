775018c0b5c90a1baa510693f8b3ad0c255a5772
Tayebe Safari <tssafri@gmail.com>
2019-03-12 12:04:24 +0330

    Added CompanyMap Class

---
+++ exercise-object-oriented/FNHClass/CompanyMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Automapping.Alterations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_object_oriented.FNHClass
+{
+    class CompanyMap:IAutoMappingOverride<Company>
+    {
+        public void Override(FluentNHibernate.Automapping.AutoMapping<Company> mapping)
+        {
+            mapping.Map(x => x.CompanyID).Not.Nullable();
+            mapping.Map(x => x.CompanyName ).Not.Nullable().Length(50);
+            mapping.Map(x => x.Industry ).Length(50);
+            mapping.Map(x => x.FaxNumber );
+            mapping.Map(x => x.Website).Length(100);
+        }
+    }
+}
