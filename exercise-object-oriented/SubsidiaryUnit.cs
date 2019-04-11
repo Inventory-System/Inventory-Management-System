6674827d29ce25678a84136c4c5d8d9f4823ba96
Mitra Safari <93safari@gmail.com>
2019-04-11 18:34:46 +0430

    Deleted exercise Project

--- exercise-object-oriented/SubsidiaryUnit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace exercise_object_oriented
-{
-    public class SubsidiaryUnit : BaseClass
-    {
-        public virtual string SubsidiaryUnitName { set; get; }
-        public virtual decimal Coefficient { get; set; }
-        public virtual Measurement measurement { get; set; }
-    }
-}
