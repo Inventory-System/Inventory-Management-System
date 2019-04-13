d0dba44ee1f36d9ba0f64772af44d591346418a9
Tayebe Safari <tssafri@gmail.com>
2019-04-13 15:35:13 +0430

    Added new Purchase.Service Calss.

---
+++ Purchase.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchase.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
