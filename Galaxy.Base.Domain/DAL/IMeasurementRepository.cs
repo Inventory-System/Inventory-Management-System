b1560f0c8ec2d4f44a87163e802b722322ea27fb
Tayebe Safari <tssafri@gmail.com>
2019-04-15 12:48:08 +0430

    Created IMeasurement Repository

---
+++ Galaxy.Base.Domain/DAL/IMeasurementRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy.Base.Domain.DAL
+{
+    interface IMeasurementRepository:IRepository<Measurement>
+    {
+    }
+}
