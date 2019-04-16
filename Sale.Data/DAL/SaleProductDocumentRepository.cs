d2d9d2b3e1fd388b141f36a32ff7575b657a537f
HMoradiRad <moradirad007@gmail.com>
2019-04-16 14:30:50 +0430

    create repository for project sale

---
+++ Sale.Data/DAL/SaleProductDocumentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Data.DAL;
+using Galaxy.Base.Domain.DAL;
+using NHibernate;
+using Sale.Domain.DAL;
+
+namespace Sale.Data.DAL
+{
+    public class SaleProductDocumentRepository:Repository<SaleProductDocument>,ISaleProductDocumentRepository
+    {
+        public SaleProductDocumentRepository(ISession session, ITransaction transaction) : base(session, transaction)
+        {
+            
+        }
+    }
+}
