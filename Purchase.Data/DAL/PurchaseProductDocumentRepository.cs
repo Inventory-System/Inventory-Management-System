39c58467036adc31af492135b2f2b9ad5dfb350b
Tayebe Safari <tssafri@gmail.com>
2019-04-16 12:14:21 +0430

    Rename PurchaseRepositories

---
+++ Purchase.Data/DAL/PurchaseProductDocumentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Data.DAL;
+using NHibernate;
+using Purchase.Domain.DAL;
+
+namespace Purchase.Data.DAL
+{
+    class PurchaseProductDocumentRepository : Repository<PurchaseProductDocumentRepository>, IPurchaseProductDocumentRepository
+    {
+        public PurchaseProductDocumentRepository(ISession session, ITransaction transaction) : base(session, transaction)
+        {
+        }
+    }
+}
