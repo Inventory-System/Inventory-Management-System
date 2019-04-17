2c845798bfc5f54918cf67629c89139a6789b956
HMoradiRad <moradirad007@gmail.com>
2019-04-17 15:10:46 +0430

    create folder DAL and Update folder dal

---
+++ Sale.Domain/DAL/ISaleUnitOfWorkRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Domain.DAL;
+
+namespace Sale.Domain.DAL
+{
+    public interface ISaleUnitOfWorkRepository :IUnitOfWork,IDisposable
+    {
+        
+        ISaleDocumentRepository SaleDocumentRepository { get; }
+        ISaleProductDucumentRepository ProductDucumentRepository { get; }
+    }
+}
