fe48f7debd23bb953183c32c7c0fda3d0c854147
Pedrampzh <you@example.com>
2019-04-19 11:52:52 -0700

    AddRepositoryIntoInventory

---
+++ Inventory.Data/DAL/InventoryDocumentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Galaxy.Base.Data.DAL;
+using Inventory.Domain;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data.DAL
+{
+   public  class InventoryDocumentRepository:Repository<InventoryDocument>:IInventoryDocumentRepository
+    {
+        public InventoryDocumentRepository(ISession session , ITransaction transaction) : base(session , transaction)
+        {
+
+        }
+    }
+}
---
+++ Inventory.Data/DAL/InventoryProductDocumentMap.cs
@@ -0,0 +1,14 @@
+﻿using Galaxy.Base.Data.DAL;
+using Inventory.Domain;
+using NHibernate;
+
+namespace Inventory.Data.DAL
+{
+    public class InventoryProductDocumentRepository:Repository<InventoryProductDocument> : IInventoryProductDocumentRepository
+    {
+        InventoryProductDocumentRepository(ISession session , ITransaction):base(session , transaction)
+        {
+
+        }
+    }
+}
