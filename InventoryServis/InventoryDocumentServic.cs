fb808448d9ff02b1b5137663479b5672a0c22b1e
pedrampzh <pzhpedram@gmail.com>
2019-04-24 09:04:39 +0430

    Added Inventory Services

---
+++ InventoryServis/InventoryDocumentServic.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Domain.DAL;
+using Galaxy.Base.Service;
+
+namespace InventoryServis
+{
+    public class InventoryDocumentServic : Service<InventoryDocument>
+    {
+        public InventoryDocumentServic(IRepository<InventoryDocument> repository): base(repository)
+        {
+ 
+        }
+    }
+}
