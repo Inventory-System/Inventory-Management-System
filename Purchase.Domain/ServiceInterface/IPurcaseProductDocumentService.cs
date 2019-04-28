031fe75cbd94784739a2aa3503426b47a56dbf85
Tayebe Safari <tssafri@gmail.com>
2019-04-28 11:50:30 +0430

    Created Purchase Service

---
+++ Purchase.Domain/ServiceInterface/IPurcaseProductDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Domain;
+using Galaxy.Base.Domain.ServiceInterfaces;
+
+namespace Purchase.Domain.ServiceInterface
+{
+    public interface IPurcaseProductDocumentService:IService<PurchaseProductDocument>
+    {
+    }
+}
---
+++ Purchase.Service/PurchaseProductDocumentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Domain.DAL;
+using Galaxy.Base.Service;
+
+namespace Purchase.Service
+{
+    public class PurchaseProductDocumentService:Service<PurchaseProductDocument>
+    {
+        public PurchaseProductDocumentService(IRepository<PurchaseProductDocument> repository) : base(repository)
+        {
+        }
+    }
+}
