77ba0274b99c7ee4f0ea170485137b9e19bed097
Tayebe Safari <tssafri@gmail.com>
2019-04-28 10:29:47 +0430

    Added DependenctyInjection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Container = new WindsorContainer(); 
+        }
+    }
+}
