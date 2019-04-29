5bfeb01d542f5a07dfe98e57a051ac797aecb640
HMoradiRad <moradirad007@gmail.com>
2019-04-29 17:14:31 +0430

    create interface fo project sale

---
+++ Sale.Service/SaleDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Service;
+
+namespace Sale.Service
+{
+   public class SaleDocumentService:Service<SaleDocument>,
+    {
+
+    }
+}
---
+++ Sale.Service/SaleProductDocumentServicecs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Service;
+using Sale.Domain.DAL;
+
+namespace Sale.Service
+{
+   public class SaleProductDocumentServicecs:Service<SaleProductDocument>,ISaleProductDucumentRepository
+    {
+
+    }
+}
