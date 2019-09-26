217443611e127d8bd1b053a0218d5ba809170b60
Mitra Safari <93safari@gmail.com>
2019-09-26 14:19:10 +0330

    Refactor DependencyInjection

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,49 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
-using Galaxy.Base.Service;
-using Galaxy.Base.Domain.ServiceInterfaces;
-using Galaxy.Base.Data.DAL;
-using Galaxy.Base.Domain.DAL;
 using Galaxy.Base.Domain;
+using Galaxy.Base.Domain.ControllerInterfaces;
+using Galaxy.Base.Domain.ServiceInterfaces;
 
 namespace DependencyInjection
 {
     
     class Program
     {
-        //private static WindsorContainer _windsorContainer;
+        private static WindsorContainer _windsorContainer;
         public static void Main(string[] args)
         {
-          //  InitializeWindsor();
-          //  _windsorContainer.Register(Component.For(typeof(IService<>)).ImplementedBy(typeof(Service<>)).LifestyleTransient());
-          //  _windsorContainer.Register(Component.For(typeof(IRepository<>)).ImplementedBy(typeof(Repository<>)));
-          //  _windsorContainer.Register(Component.For(typeof(IUnitOfWork)).ImplementedBy(typeof(UnitOfWork)));
-          ////  _windsorContainer.Resolve<ProductService>();
+           
+            // _windsorContainer = new WindsorContainer();
+
+            // _windsorContainer.Install(FromAssembly.Containing<DependencyInstaller>());
+      
+            //var root =  _windsorContainer.Resolve<IMeasurementController>();
+            
+
+            //var measurmentCm = new Measurement { Code = 1, Name = "Kilogram" };
+            //root.Create(measurmentCm);//
+            //var product= new Product{Code=1, measurement=measurmentCm,Name="Youghart"};
+            //root.Create(product);
 
             
 
+            Console.ReadLine();
+    
+
+
+
         }
 
-        //protected void Application_End()
-        //{
-        //    if (_windsorContainer != null)
-        //    {
-        //        _windsorContainer.Dispose();
-        //    }
-        //}
-
-        //private static void InitializeWindsor()
-        //{
-        //    //_windsorContainer = new WindsorContainer();
-        //    //_windsorContainer.Install(FromAssembly.This());
-
-        //   // ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(_windsorContainer.Kernel));
-        //}
     }
 }
