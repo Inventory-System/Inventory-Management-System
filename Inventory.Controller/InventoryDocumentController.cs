1f2b2dd12d5fef1e101d78b1cea395138499f90c
Mitra Safari <93safari@gmail.com>
2019-10-04 23:31:49 +0330

    Added Inventory Controllers

---
+++ Inventory.Controller/InventoryDocumentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Controller;
+using Galaxy.Base.Domain.ServiceInterfaces;
+using Inventory.Domain;
+using Inventory.Domain.ControllerInterface;
+
+namespace Inventory.Controller
+{
+    public class InventoryDocumentController: Controller<InventoryDocument>,IInventoryDocumentController
+    {
+        public InventoryDocumentController(IService<InventoryDocument> Service) : base(Service)
+        {
+        }
+    }
+}
