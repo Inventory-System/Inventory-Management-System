320e32cb3ef7834a28ac6302f4d9fff9760da31c
Mitra Safari <93safari@gmail.com>
2019-10-05 00:20:45 +0330

    Added Purchase Controller

---
+++ Purchase.Controller/PurchaseDocumentController.cs
@@ -0,0 +1,18 @@
+﻿using Galaxy.Base.Controller;
+using Purchase.Domain.ControllerInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Domain.ServiceInterfaces;
+
+namespace Purchase.Controller
+{
+    public class PurchaseDocumentController: Controller<PurchaseDocument>, IPurchaseDocumentController
+    {
+        public PurchaseDocumentController(IService<PurchaseDocument> Service) : base(Service)
+        {
+        }
+    }
+}
---
+++ Purchase.Controller/PurchaseProductDocumentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Controller;
+using Galaxy.Base.Domain.ServiceInterfaces;
+using Purchase.Domain.ControllerInterface;
+
+namespace Purchase.Controller
+{
+    class PurchaseProductDocumentController:Controller<PurchaseProductDocument> , IPurcahseProductDocumentController
+    {
+        public PurchaseProductDocumentController(IService<PurchaseProductDocument> Service) : base(Service)
+        {
+        }
+    }
+}
