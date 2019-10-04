c2400c0bce171ce74d6c7d585949cd3c566c1d8a
Mitra Safari <93safari@gmail.com>
2019-10-05 00:41:55 +0330

    Added Sale Controller

---
+++ Sale.Controller/SaleDocuementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaxy.Base.Controller;
+using Galaxy.Base.Domain.ServiceInterfaces;
+using Sale.Domain.ControllerInterface;
+
+namespace Sale.Controller
+{
+    class SaleDocuementController: Controller<SaleDocument>,ISaleDocumentController
+    {
+        public SaleDocuementController(IService<SaleDocument> Service) : base(Service)
+        {
+        }
+    }
+}
---
+++ Sale.Controller/SaleProductDocumentController.cs
@@ -0,0 +1,18 @@
+﻿using Galaxy.Base.Controller;
+using Galaxy.Base.Domain.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sale.Domain.ControllerInterface;
+
+namespace Sale.Controller
+{
+    public class SaleProductDocumentController:Controller<SaleProductDocument>, ISaleProductDocumentController
+    {
+        public SaleProductDocumentController(IService<SaleProductDocument> Service) : base(Service)
+        {
+        }
+    }
+}
